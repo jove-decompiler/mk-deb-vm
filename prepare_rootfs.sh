b8feb65d3bb65fde238902c37d2db1b115721cfd
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2023-07-27 23:45:11 -0400

    create /etc/hosts

--- prepare_rootfs.sh
+++ prepare_rootfs.sh
@@ -10,6 +10,16 @@ EOF
 
 echo "$hostname" > rootfs/etc/hostname
 
+cat > rootfs/etc/hosts <<EOF
+127.0.0.1       localhost
+
+127.0.1.1       $hostname.mydomain.org $hostname
+
+::1             localhost ip6-localhost ip6-loopback
+ff02::1         ip6-allnodes
+ff02::2         ip6-allrouters
+EOF
+
 cat > rootfs/etc/systemd/network/enp.network <<EOF
 [Match]
 Name=enp*
