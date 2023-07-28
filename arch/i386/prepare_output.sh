bd385e4f9eb72c2becb06e9b65652e8f0306e3a1
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2023-07-27 23:08:02 -0400

    (i386) virtio-blk-device -> virtio-blk-pci, virtio-net-device -> virtio-net-pci

--- arch/i386/prepare_output.sh
+++ arch/i386/prepare_output.sh
@@ -9,9 +9,9 @@ qemu-system-i386 -M pc \\
                  -initrd initrd.img \\
                  -m 2048 \\
                  -drive if=none,format=raw,file=vm.raw,media=disk,id=hd0 \\
-                 -device virtio-blk-device,drive=hd0 \\
+                 -device virtio-blk-pci,drive=hd0 \\
                  -netdev user,id=net0,hostfwd=tcp::10022-:22 \\
-                 -device virtio-net-device,netdev=net0 \\
+                 -device virtio-net-pci,netdev=net0 \\
                  -append "nokaslr nr_cpus=1 root=/dev/vda1 rootwait console=ttyS0,115200" \\
                  -serial pty \\
                  -nographic
