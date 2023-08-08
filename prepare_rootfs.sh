a25c4669044db83d80a0a4afe00eaeea2402cae7
Anthony Eden <anthony.louis.eden@gmail.com>
2023-08-08 05:47:50 +0000

    install systemd-coredump

--- prepare_rootfs.sh
+++ prepare_rootfs.sh
@@ -1,4 +1,4 @@
-debootstrap --include=${deb_extra_packages}nano,iproute2,initramfs-tools,openssh-server,locales,xz-utils,zstd --arch=$deb_arch $deb_suite rootfs $deb_mirror || { echo >&2 "debootstrap failed." ; cat rootfs/debootstrap/debootstrap.log ; close_rootfs ; exit 1; }
+debootstrap --include=${deb_extra_packages}nano,iproute2,initramfs-tools,openssh-server,locales,xz-utils,zstd,systemd-coredump --arch=$deb_arch $deb_suite rootfs $deb_mirror || { echo >&2 "debootstrap failed." ; cat rootfs/debootstrap/debootstrap.log ; close_rootfs ; exit 1; }
 
 root_uuid=$(blkid -s UUID -o value ${loopdev}p1)
 swap_uuid=$(blkid -s UUID -o value ${loopdev}p2)
