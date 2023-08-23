f07d27b339f40fe29c3bcd3857e8e5cf2d991637
Aoto Tokizawa <aoto.tokizawa@gmail.com>
2023-08-23 19:43:00 -0400

    pass nosmp instead of nr_cpus=1 to kernel

--- arch/aarch64/prepare_output.sh
+++ arch/aarch64/prepare_output.sh
@@ -12,7 +12,7 @@ qemu-system-aarch64 -M virt \\
                     -device virtio-blk-device,drive=hd0 \\
                     -netdev user,id=net0,hostfwd=tcp::$ssh_port-:22 \\
                     -device virtio-net-device,netdev=net0 \\
-                    -append "nokaslr nr_cpus=1 root=/dev/vda1 rootwait console=ttyAMA0,115200" \\
+                    -append "nokaslr nosmp root=/dev/vda1 rootwait console=ttyAMA0,115200" \\
                     -serial $serial_arg \\
                     -nographic
 EOF
--- arch/mips/prepare_output.sh
+++ arch/mips/prepare_output.sh
@@ -11,7 +11,7 @@ qemu-system-mips -M malta \\
                  -drive if=ide,format=raw,file=vm.raw,media=disk \\
                  -netdev user,id=net0,hostfwd=tcp::$ssh_port-:22 \\
                  -device e1000,netdev=net0 \\
-                 -append "nokaslr nr_cpus=1 root=/dev/sda1 rootwait console=ttyS0,115200" \\
+                 -append "nokaslr nosmp root=/dev/sda1 rootwait console=ttyS0,115200" \\
                  -serial $serial_arg \\
                  -nographic
 EOF
