b3330db8057729b5350d68a318f47baa6632e59c
Anthony Eden <anthony.louis.eden@gmail.com>
2024-12-24 13:20:56 -0800

    default xfs mount options in fstab

--- prepare_rootfs.sh
+++ prepare_rootfs.sh
@@ -4,8 +4,8 @@ root_uuid=$(blkid -s UUID -o value ${newroot}${loopdev}p1)
 swap_uuid=$(blkid -s UUID -o value ${newroot}${loopdev}p2)
 
 cat > rootfs/etc/fstab <<EOF
-UUID=$root_uuid /    xfs rw,relatime  0 1
-UUID=$swap_uuid none swap defaults     0 0
+UUID=$root_uuid /    xfs  defaults  0 1
+UUID=$swap_uuid none swap defaults  0 0
 EOF
 
 echo "$hostname" > rootfs/etc/hostname
